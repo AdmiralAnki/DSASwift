e71a42cbb85a8e065047eb7ecc15db01d438f6a6
Ankith <ankithkirti@gmail.com>
2024-01-17 21:52:02 +0530

    rebase

--- Arrays.playground/Contents.swift
+++ Arrays.playground/Contents.swift
@@ -2,5 +2,21 @@ import UIKit
 
 //MARK: Sorting
 
-let array = [2,1,5,79,22,89,23,98,23,54,234,342
-]
+var array = [2,1,5,79,22,89,23,98,23,54,234,342]
+
+//Reversing
+
+func reverseArray<T>(array: inout [T]){
+    var start = 0
+    var end = array.count-1
+    
+    while start < end{
+        array.swapAt(start, end)
+        start+=1
+        end-=1
+    }
+}
+
+reverseArray(array: &array)
+
+print(array)
